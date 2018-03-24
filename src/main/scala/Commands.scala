222da96e4b334dcf1d23662cdff27e7c856d0799
Gosha20 <gosha3548@gmail.com>
2018-03-24 17:00:25 +0500

    a

--- src/main/scala/Commands.scala
+++ src/main/scala/Commands.scala
@@ -8,22 +8,27 @@ object Commands {
                          endTime: DateTime) extends Command {
     override def execute(): String = {
       "create poll with params " + name + anon + resShow + startTime + endTime
-
     }
   }
+
   case class List() extends Command {
     override def execute(): String = "some list polls"
   }
+
   case class DeletePoll(id : Int)extends Command{
     override def execute(): String = "delete poll " + id
   }
+
   case class StartPoll(id : Int)extends Command{
     override def execute(): String = "start poll " + id
   }
+
   case class StopPoll(id : Int)extends Command{
     override def execute(): String = "stop poll " + id
   }
+  
   case class Result(id : Int)extends Command{
     override def execute(): String = "result " + id
   }
+
 }
