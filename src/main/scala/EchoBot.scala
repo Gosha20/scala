15c26468bfbb07baa0eebfa18407f98b454ef340
Gosha20 <gosha3548@gmail.com>
2018-05-14 10:50:26 +0500

    com

--- src/main/scala/EchoBot.scala
+++ src/main/scala/EchoBot.scala
@@ -7,15 +7,16 @@ import info.mukel.telegrambot4s.models._
   * Echo, ohcE
   */
 object EchoBot extends TelegramBot with Polling {
-  lazy val token = "579195535:AAF_xTvql4h3XRCAvB9Obhs016fb17wY7yo"
-  val users = List[UserHandler]
+  lazy val token = "579195535:AAELB3_J5izTN5sR5X9CixO0G6xzI0GVt8A"
+//  val users = List[UserHandler]
   override def receiveMessage(msg: Message): Unit = {
+    print(msg)
     for (text <- msg.text) {
-      val user = User(msg.source.toString)
-      val userHand = UserHandler(user)
-
-      val cmd = userHand.performCommand(msg.text.get)
-      request(SendMessage(msg.source, cmd))
+//      val user = User(msg.source.toString)
+//      val userHand = UserHandler(user)
+//
+//      val cmd = userHand.performCommand(msg.text.get)
+      request(SendMessage(msg.source, text))
     }
   }
 //
